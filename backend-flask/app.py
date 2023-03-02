9acc8f7f4640f44d66c887504133042bc58b6793
wasem-aslam <wasem_aslam@hotmail.com>
2023-03-02 12:13:15 +0000

    xray added + Xray functionality added.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,15 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# X-RAY ----------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+# X-RAY ----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
