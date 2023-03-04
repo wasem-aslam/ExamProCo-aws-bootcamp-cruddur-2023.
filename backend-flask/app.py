32ccdb51cd29ca4edd4c76ec24c7f2dd69d8d36a
wasem-aslam <wasem_aslam@hotmail.com>
2023-03-04 22:57:32 +0000

    Adjusted the code to work for Rollbar.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,17 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+# Rollbar -----
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
+# Cloudwatch ----- 
+import watchtower
+import logging
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -14,49 +25,65 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
-# HoneyComb ------
+# Honeycomb ----------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter,SimpleSpanProcessor
 
-# AWS X-Ray -------
-#from aws_xray_sdk.core import xray_recorder
-#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+# X-Ray -----
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-# Rollbar -----
-import rollbar
-import rollbar.contrib.flask
-from flask import got_request_exception
+# Cloudwatch ----- 
+from time import strftime
 
-# HoneyComb -------
+# Honeycomb ----------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+
+# X-Ray -----
+# Remvoed for cost saving
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+# show this in the logs within the backend-flask app (STDOUT) 
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter)
+provider.add_span_processor(simple_processor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
-
-# CloudWatch Logs -----
-import watchtower
-import logging
-from time import strftime
-
 
 
 app = Flask(__name__)
 
-# HoneyComb -------
+
+# X-Ray -----
+# Remvoed for cost saving
+# XRayMiddleware(app, xray_recorder)
+
+
+# Honeycomb ----------
+# Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
-# AWS X-Ray -------
-#xray_url = os.getenv("AWS_XRAY_URL")
-#xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-#XRayMiddleware(app, xray_recorder)
-
+# Cloudwatch -----
+# Configuring Logger to Use CloudWatch
+# Removed the LOGGER to avoid the Cost 
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("Test log")
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -70,27 +97,15 @@ cors = CORS(
 )
 
 
-# CloudWatch --------
-# Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("some message")
+# Cloudwatch ----- Error message
+# Disabled for cost saving
+# @app.after_request
+# def after_request(response):
+#    timestamp = strftime('[%Y-%b-%d %H:%M]')
+#    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#    return response
 
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    return response
-
-@app.route('/rollbar/test')
-def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
-
+# Rollbar -----
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 @app.before_first_request
 def init_rollbar():
@@ -107,6 +122,14 @@ def init_rollbar():
 
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+# Rolebar -----
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -145,8 +168,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
-  LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
+  data = HomeActivities.run() # fix for Cloudwatch Log and removed now to avoid cost
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
